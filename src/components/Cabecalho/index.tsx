591f534d97004c76677ee6b361be87471025ad62
Leonardo Abril <leoabril98@gmail.com>
2024-03-05 19:47:52 -0300

    estilização utilizando o Styled Components

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,10 @@
-import styles from './Cabecalho.module.css'
+import React from 'react'
+import { HeaderWrapper } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <HeaderWrapper>
     <h1>EBAC Jobs</h1>
-  </header>
+  </HeaderWrapper>
 )
 
 export default Cabecalho
